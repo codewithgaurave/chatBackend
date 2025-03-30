98a14c118f36945e6f1f9daa68f0e5af29f8bd9c
codewithgaurave <digitalgurucse@gmail.com>
2025-03-30 10:03:22 +0530

    some changes occur

--- server.js
+++ server.js
@@ -75,6 +75,10 @@ const io = initSocket(server);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("Gaurav Gupta");
+});
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
